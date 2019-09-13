afabd3918915d6b11a56ab4cf2a1bd0d6b335cdb
launderex619 <carlos12150@hotmail.com>
2019-09-12 20:21:08 -0500

    Se creo el main activity, algunos eventos de botones, vistas basicas (falta completar el recicler view, el reconocimiento de archivo)

--- app/src/main/java/com/example/sulemaia/MainActivity.java
+++ app/src/main/java/com/example/sulemaia/MainActivity.java
@@ -1,55 +1,177 @@
 package com.example.sulemaia;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.sulemaia.Helper.Constants;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnCancel;
+    private ImageButton btnOk;
+    private Button btnAddFile;
+    private TextView tvTextSelectedMap;
+    private TableLayout tlTableMap;
+    private String fileDataUnFiltered = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //instancia de elementos del xml
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        TextView tvActivityTitle = findViewById(R.id.toolbar_title);
+        btnCancel = findViewById(R.id.btn_cancel);
+        btnOk = findViewById(R.id.btn_ok);
+        btnAddFile = findViewById(R.id.btn_add_file);
+        tvTextSelectedMap = findViewById(R.id.text_not_selected_file);
+        tlTableMap = findViewById(R.id.table_map);
+        //clase creada por mi para recibir todos los eventos de los botones (clase = ButtonActions)
+        View.OnClickListener buttonActions = new ButtonActions(this);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        tvActivityTitle.setText(getString(R.string.main_activity_title));
+
+        //asigno los eventos escuchadores para asignar las acciones de los botones
+        btnCancel.setOnClickListener(buttonActions);
+        btnAddFile.setOnClickListener(buttonActions);
+        btnOk.setOnClickListener(buttonActions);
+
+        btnCancel.setVisibility(View.INVISIBLE);
+        btnOk.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Constants.REQUEST_FILE_READ_EXTERNAL && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Uri fileUri = data.getData();
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(fileUri);
+                    int content = inputStream.read();
+                    while (content != -1) {
+                        fileDataUnFiltered += (char) content;
+                        content = inputStream.read();
+                    }
+                    (new AlertDialog.Builder(this)
+                            .setTitle("Contenido del Archivo")
+                            .setMessage(fileDataUnFiltered)
+                            .setPositiveButton("OK", null)
+                            .create()).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    (new AlertDialog.Builder(this)
+                            .setTitle("Contenido del Archivo")
+                            .setMessage("El archivo no es valido")
+                            .setPositiveButton("OK", null)
+                            .create()).show();
+                }
+            } else {
+                (new AlertDialog.Builder(this)
+                        .setTitle("Error de archivo")
+                        .setMessage("No se selecciono ningun archivo")
+                        .setPositiveButton("OK", null)
+                        .create()).show();
             }
-        });
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void startIntentToReadFile() {
+        Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFile.setType("*/*");
+        chooseFile = Intent.createChooser(chooseFile, "Choose a file");
+        startActivityForResult(chooseFile, Constants.REQUEST_FILE_READ_EXTERNAL);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class ButtonActions implements View.OnClickListener,
+            ActivityCompat.OnRequestPermissionsResultCallback {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        private Activity activity;
+
+        ButtonActions(Activity activity) {
+            this.activity = activity;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onClick(View v) {
+            if (v == btnCancel) {
+
+            } else if (v == btnAddFile) {
+                fileDataUnFiltered = "";
+                if (isPermissionGranted()) {
+                    startIntentToReadFile();
+                }
+            } else if (v == btnOk) {
+
+            }
+        }
+
+        private boolean isPermissionGranted() {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Permission is not granted
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(getApplicationContext(),
+                            getString(R.string.read_permission),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(activity,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        Constants.MY_PERMISSIONS_REQUEST_READ_EXTERNAL);
+                return false;
+            }
+
+            // Permission has already been granted
+            return true;
+
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode) {
+                case Constants.MY_PERMISSIONS_REQUEST_READ_EXTERNAL:
+                    if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Snackbar.make(getWindow().getDecorView().getRootView(), R.string.read_permission_granted,
+                                Snackbar.LENGTH_SHORT)
+                                .show();
+                        startIntentToReadFile();
+                    } else {
+                        // Permission request was denied.
+                        Snackbar.make(getWindow().getDecorView().getRootView(), R.string.read_permission_denied,
+                                Snackbar.LENGTH_SHORT)
+                                .show();
+                    }
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sulemaia/Tutorial.java
+++ app/src/main/java/com/example/sulemaia/Tutorial.java
@@ -18,7 +18,7 @@ public class Tutorial extends AppCompatActivity {
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
