7de1b4b1dc287e65c2e3c93390ae09e5518e2b2a
TheGenesisX <alejandrosmoya@gmail.com>
2019-10-04 21:19:15 -0500

    Agregado el tutorial

--- app/src/main/java/com/example/sulemaia/Activity/Tutorial.java
+++ app/src/main/java/com/example/sulemaia/Activity/Tutorial.java
@@ -1,6 +1,7 @@
 package com.example.sulemaia.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,10 +12,22 @@ import com.example.sulemaia.R;
 
 public class Tutorial extends AppCompatActivity {
 
+    ViewPager viewPager;
+    int images[] = {R.drawable.tut1,R.drawable.tut2,R.drawable.tut3,R.drawable.tut4,R.drawable.tut5,R.drawable.tut6,R.drawable.tut7,
+                    R.drawable.tut8,R.drawable.tut9,R.drawable.tut10,R.drawable.tut11,R.drawable.tut12,R.drawable.tut13,R.drawable.tut14,
+                    R.drawable.tut15,R.drawable.tut16};
+
+    MyCustomPagerAdapter myCustomPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        viewPager = findViewById(R.id.pager);
+
+        myCustomPagerAdapter = new MyCustomPagerAdapter(Tutorial.this, images);
+        viewPager.setAdapter(myCustomPagerAdapter);
 
         Button btnMain = (Button) findViewById(R.id.toMain);
         btnMain.setOnClickListener(new View.OnClickListener() {
